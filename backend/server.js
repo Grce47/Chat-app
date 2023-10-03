8c4dbda97799a70b985aeb981d8ccfa067e8f806
Grace Sharma <gracesharma47@gmail.com>
2023-10-03 20:59:01 +0530

    backend complete test not done

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("good");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
